dfb542c571b984775da44ecf3664acbdbe63189f
Phong Nguyen <nhphong1406@gmail.com>
2017-08-06 18:40:24 +0700

    Removes KC_MENU mappings

--- keymap.c
+++ keymap.c
@@ -54,11 +54,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_CUSTOM] = {
-    {KC_EQL,         KC_1,        KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,                KC_MINS},
-    {KC_TAB,         KC_Q,        KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,                KC_BSPC},
-    {ALT_T(KC_ESC),  KC_A,        KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    LT(_MOUSE, KC_SCLN), GUI_T(KC_QUOT)},
-    {KC_LSFT,        CTL_T(KC_Z), KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  CTL_T(KC_SLSH),      SFT_T(KC_ENT)},
-    {RSFT_T(KC_GRV), KC_LCTL,     KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,               KC_RGHT}
+    {KC_EQL,         KC_1,        KC_2,          KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,                KC_MINS},
+    {KC_TAB,         KC_Q,        KC_W,          KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,                KC_BSPC},
+    {ALT_T(KC_ESC),  KC_A,        KC_S,          KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    LT(_MOUSE, KC_SCLN), GUI_T(KC_QUOT)},
+    {KC_LSFT,        CTL_T(KC_Z), KC_X,          KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  CTL_T(KC_SLSH),      SFT_T(KC_ENT)},
+    {RSFT_T(KC_GRV), KC_LCTL,     ALT_T(KC_APP), KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,               KC_RGHT}
 },
 
 /* Qwerty
@@ -149,20 +149,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |MClick| MSUp |RClick| APP  | MENU |RClick| SUp  |Mclick|      |      |
+ * |      |      |MClick| MSUp |RClick|      |      |RClick| SUp  |Mclick|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |LClick|MSLeft|MSDown|MRight|LClick|LClick|SLeft |SDown |SRight|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      | MENU | APP  |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_MOUSE] = {
     {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-    {_______, _______, KC_BTN3, KC_MS_U, KC_BTN2, KC_APP,  KC_MENU, KC_BTN2, KC_WH_U, KC_BTN3, _______, _______},
+    {_______, _______, KC_BTN3, KC_MS_U, KC_BTN2, _______, _______, KC_BTN2, KC_WH_U, KC_BTN3, _______, _______},
     {_______, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1, KC_BTN1, KC_WH_L, KC_WH_D, KC_WH_R, _______, _______},
-    {_______, _______, _______, _______, _______, KC_MENU, KC_APP,  _______, _______, _______, _______, _______},
+    {_______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______},
     {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 },
 
